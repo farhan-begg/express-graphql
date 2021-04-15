e532d5baaa10c656832d5c3b7c2bf04f065dcd12
NoblePrinceii <farhan.begg@students.makeschool.com>
2021-04-15 13:17:57 -0700

    challenge 10 working

--- server.js
+++ server.js
@@ -5,19 +5,70 @@ const { buildSchema } = require('graphql')
 
 // Create a schema
 const schema = buildSchema(`
-type About {
-  message: String!
+
+enum Species {
+    Dog 
+    Cat
+    Frog
 }
 
+type Pet {
+    name: String!
+    species: Species! # use an enum!
+}
+type Time{
+    hour: String!
+    minute: String!
+    second: String!
+}
+
+
 type Query {
-  getAbout: About
-}`)
+    allPets: [Pet!]! # returns a collection of Pet
+    getPet(index: Int!): Pet
+    firstPet: Pet
+    getTime: Time
+    getRandom(range: Int!): Int
+}
+  
+
+
+
+ `)
+
+// challenge 1
+ const petList = [
+    { name: 'Fluffy', species: 'Dog' },
+    { name: 'Sassy', species: 'Cat' },
+    { name: 'Goldberg', species: 'Frog' }
+]
 
 // Define a resolver
 const root = {
-    getAbout: () => {
-      return { message: 'Hello World' }
-    }
+    allPets: () => {
+        return petList;
+    },
+    getPet: ({ index }) => { // index is a param from the query
+        return petList[index]
+    },
+    firstPet: () => {
+        return petList[0]
+    },
+
+    getTime: () => {
+        return {
+          hour: new Date().getHours().toString() - 12,
+          minute: new Date().getMinutes().toString(),
+          second: new Date().getSeconds().toString(),
+        };
+        
+    },
+
+    getRandom:  ({range})  => {
+        return Math.floor(Math.random() * range);
+     },
+    
+    
   }
 
 // Create an express app
