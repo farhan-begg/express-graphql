ba1a6f2df983952b715a83356055e6d91cf5546a
NoblePrinceii <farhan.begg@students.makeschool.com>
2021-04-15 13:56:15 -0700

    challenge 14 completed

--- server.js
+++ server.js
@@ -16,11 +16,18 @@ type Pet {
     name: String!
     species: Species! # use an enum!
 }
-type Time{
+type Time {
     hour: String!
     minute: String!
     second: String!
 }
+
+type Dice {
+    total: Int!
+    sides: Int!
+    rolls: [Int]
+}
+
 
 
 type Query {
@@ -29,14 +36,15 @@ type Query {
     firstPet: Pet
     getTime: Time
     getRandom(range: Int!): Int
+    getRoll(sides: Int!, rolls: Int!): Dice
+    getCount: Int!
+    petsInRange(start: Int!, count: Int!): [Pet!]!
+    getPetBySpecies(species: String!): [Pet!]!
+
 }
   
-
-
-
  `)
 
-// challenge 1
  const petList = [
     { name: 'Fluffy', species: 'Dog' },
     { name: 'Sassy', species: 'Cat' },
@@ -61,12 +69,55 @@ const root = {
           minute: new Date().getMinutes().toString(),
           second: new Date().getSeconds().toString(),
         };
-        
+
     },
+
 
     getRandom:  ({range})  => {
         return Math.floor(Math.random() * range);
      },
+
+
+     getRoll: ({ sides, rolls }) => {
+        let totalCount = 0;
+        let diceRoll = [];
+        for (let i = 0; i < rolls; i += 1) {
+          number = Math.floor(Math.random() * sides);
+          diceRoll.push(number);
+          totalCount += number;
+        }
+        return {
+          total: totalCount,
+          sides: sides,
+          rolls: diceRoll,
+        };
+      },
+
+      getCount: () => {
+          return petList.length
+      },
+
+      petsInRange: ({ start, count}) => {
+          let totalPets = [];
+
+          for(i = 0; i <= count; i += 1){
+              totalPets.push(petList[i])
+          }
+          return totalPets
+      },
+
+      getPetBySpecies: ({ species }) => {
+        return petList.filter(item => item.species === species)
+      },
+
+   
+
+      
+
+      
+    
+    
+        
     
     
   }
