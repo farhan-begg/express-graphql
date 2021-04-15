2979ee4e2947ca71c09061bd36380fa08c2f10e6
NoblePrinceii <farhan.begg@students.makeschool.com>
2021-04-15 10:29:52 -0700

    setup

---
+++ server.js
@@ -0,0 +1,38 @@
+// Import dependancies
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const { buildSchema } = require('graphql')
+
+// Create a schema
+const schema = buildSchema(`
+type About {
+  message: String!
+}
+
+type Query {
+  getAbout: About
+}`)
+
+// Define a resolver
+const root = {
+    getAbout: () => {
+      return { message: 'Hello World' }
+    }
+  }
+
+// Create an express app
+const app = express()
+
+// Define a route for GraphQL
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+  }))
+  
+
+  // Start this app
+const port = 4000
+app.listen(port, () => {
+  console.log(`Running on port: ${port}`)
+})
